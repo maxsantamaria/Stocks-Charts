8b596bbd555583aaf8006a6ab2e0913222f86823
mesantamaria <mesantamaria@uc.cl>
2020-05-12 14:54:07 -0400

    With Google Charts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import socketIOClient from "socket.io-client";
+import Chart from "react-google-charts";
 import './App.css';
+import LightWeightChart from './LightWeightChart';
+
+
 
 const PROTOCOL = 'wss://';
 const SERVER = 'le-18262636.bitzonte.com';
@@ -11,25 +15,40 @@ function App() {
   const [socket, setSocket] = useState(null);
   const [buttonText, setButtonText] = useState("Desconectarse")
   const [stocksValues, setStocksValues] = useState({})  // Key: Stock name, Value: List of values
+  const [initialTime, setInitialTime] = useState(Date.now())
+
+  //const [data, setData] = useState([['x', 'dogs']])
+  const [data, setData] = useState([])
+
   useEffect(() => {
     const sock = socketIOClient(PROTOCOL + SERVER, {
       path: PATH
     })
     setSocket(sock);
     sock.on('UPDATE', res => {
-      if (res.ticker in stocksValues) {
-        stocksValues[res.ticker].push(res.value)
-      }
-      else {
-        stocksValues[res.ticker] = [res.value]
-      }
-      console.log(res)
+      setStocksValues(currentData => {
+        if (res.ticker in currentData) {
+          return {
+            ...currentData,
+            [res.ticker]: [...currentData[res.ticker], [res.time - initialTime, res.value]]  
+          }
+        }
+        else {
+          return {
+            ...currentData,
+            [res.ticker]: [['x', 'Stock Value'], [res.time - initialTime, res.value]]  
+          }
+        }
+      })
+      //setData(currentData => [...currentData, [res.time, res.value]])
+      setData(currentData => [...currentData, {time: timeInSecs(res.time), value: res.value}])
     })
 
   }, [])
 
   const changeState = () => {
-    console.log(stocksValues)
+    //setData([...data, [3, 100]])
+    
     if (socket.connected) {
       socket.close()
       setButtonText('Conectarse')
@@ -42,14 +61,48 @@ function App() {
 
   return (
     <div className="App">
-      Hello world
-      {socket ? 
-        <button onClick={changeState}>
-            {buttonText}
-        </button>
-      : null}
+      <div>
+        {socket ? 
+          <button onClick={changeState}>
+              {buttonText}
+          </button>
+        : null}
+      </div>
+      <div>
+        {Object.keys(stocksValues).map(stock => (
+          <LightWeightChart data={data}/>
+        ))
+        
+        }
+      </div>
+      {/*Object.keys(stocksValues).map(stock => (
+          
+          <Chart
+            width={'600px'}
+            height={'400px'}
+            chartType="LineChart"
+            loader={<div>Loading Chart</div>}
+            data={stocksValues[stock]}
+            options={{
+              hAxis: {
+                title: 'Time',
+              },
+              vAxis: {
+                title: 'Popularity',
+              },
+            }}
+            rootProps={{ 'data-testid': '1' }}
+          />
+        ))*/}
+    
+      
+
     </div>
   );
+}
+
+const timeInSecs = (time) => {
+  return Math.round(time / 1000) - 4 * 60 * 60
 }
 
 export default App;
---
+++ src/LightWeightChart.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+import { createChart } from 'lightweight-charts';
+
+
+const LightWeightChart = ({ data }) => {
+  const [chart, setChart] = useState(null)
+  const [series, setSeries] = useState(null)
+  useEffect(() => {
+    
+    const chart = createChart('chart', { 
+      width: 600, 
+      height: 300,
+      layout: {
+        backgroundColor: '#000',
+        textColor: 'rgba(255, 255, 255, 0.8)',
+      },
+      grid: {
+        vertLines: {
+          color: 'rgba(255, 255, 255, 0.2)',
+        },
+        horzLines: {
+          color: 'rgba(255, 255, 255, 0.2)',
+        },
+      },
+      priceScale: {
+        borderColor: 'rgba(255, 255, 255, 0.8)',
+      },
+      timeScale: {
+        borderColor: 'rgba(255, 255, 255, 0.8)',
+      }, 
+    })
+    chart.timeScale().applyOptions({
+      timeVisible: true,
+      secondsVisible: true
+    })
+    const lineSeries = chart.addLineSeries();
+    lineSeries.setData(
+      data  
+    )
+
+    setSeries(lineSeries)
+    setChart(chart)
+
+  
+  }, [])
+  useEffect(() => {
+    if (series) {
+      series.setData(data)
+    }
+  }, [data])
+  return (
+    <div id={'chart'}>
+      
+    </div>
+  )
+}
+
+export default LightWeightChart
