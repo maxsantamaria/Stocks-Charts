a7c6e341a0f3dec128038ef899d8bad1caff6b87
mesantamaria <mesantamaria@uc.cl>
2020-05-12 10:04:21 -0400

    Web socket connection established

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from "socket.io-client";
 import './App.css';
 
+const PROTOCOL = 'wss://';
+const SERVER = 'le-18262636.bitzonte.com';
+const PATH = '/stocks';
+
+
 function App() {
+  const [socket, setSocket] = useState(null);
+  const [buttonText, setButtonText] = useState("Desconectarse")
+  const [stocksValues, setStocksValues] = useState({})  // Key: Stock name, Value: List of values
+  useEffect(() => {
+    const sock = socketIOClient(PROTOCOL + SERVER, {
+      path: PATH
+    })
+    setSocket(sock);
+    sock.on('UPDATE', res => {
+      if (res.ticker in stocksValues) {
+        stocksValues[res.ticker].push(res.value)
+      }
+      else {
+        stocksValues[res.ticker] = [res.value]
+      }
+      console.log(res)
+    })
+
+  }, [])
+
+  const changeState = () => {
+    console.log(stocksValues)
+    if (socket.connected) {
+      socket.close()
+      setButtonText('Conectarse')
+    }
+    else {
+      socket.open()
+      setButtonText('Desconectarse')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world
+      {socket ? 
+        <button onClick={changeState}>
+            {buttonText}
+        </button>
+      : null}
     </div>
   );
 }
